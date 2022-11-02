ced09701468f465cc317dc7b6831ed6ffb1a6239
badal-cloud <aryar6563@gmail.com>
2022-11-02 10:36:22 +0530

    4commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -8,8 +9,9 @@ function App() {
       <Navbar title="TextUtils" aboutText="About TextUtils"/>
       <div className="container my-3">
       <TextForm heading="Enter The Text To Analyse"/> 
+      {/* <About/> */}
       </div>
-      
+     
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,13 +22,46 @@ export default function TextForm(props) {
    // console.log("Onchange")
     setText(event.target.value)
 }
+
+const handleCopy=()=>{
+   var text=document.getElementById("myBox");
+   text.select();
+   navigator.clipboard.writeText(text.value);
+
+}
+
+const handleExtraSpace =()=>{
+  let newText=text.split(/[ ]+/);
+  setText(newText.join(" "))
+}
+
+const  handleColor=()=>{
+   if(myStyle.color==='black')
+   {
+    setmyStyle({
+       color: 'red'
+     
+    })
+    
+   }
+   else {
+    setmyStyle({
+      color: 'black'
+    })
+  }
+}
+const [myStyle, setmyStyle]=useState({
+   color:'black'
+})
+
+
   const [text,setText] = useState('Enter text here');
   return (
     <>
     <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="7">
+                <textarea className="form-control" value={text} style={myStyle} onChange={handleOnChange} id="myBox" rows="7">
           </textarea>
        
       </div>
@@ -40,6 +73,15 @@ export default function TextForm(props) {
           </button>
           <button className="btn btn-primary mx-2" onClick={handleClearClick}>
             Clear text
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleCopy}>
+            Copy text
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>
+            Remove Extra Space
+          </button>
+          <button className="btn btn-primary mx-2"  onClick={handleColor}>
+            Change Red Color
           </button>
     </div>
     <div className="container my-4">
